a80ef667a873ef9b487ef01f881bf4aa55bd0bf0
DenisTernovoy <denis.ternovoi@mail.ru>
2026-01-28 16:30:44 +0300

    Настроены права доступа

--- adv/views.py
+++ adv/views.py
@@ -1,9 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 
 from adv.models import Advertisement, Review
 from adv.paginators import AdvertisementPaginator
+from adv.permissions import IsOwner
 from adv.serializers import AdvertisementSerializer, ReviewSerializer
 
 
@@ -22,6 +24,24 @@ class AdvertisementViewSet(viewsets.ModelViewSet):
         except Exception:
             raise ValidationError({"detail": "Такой объект уже существует."})
 
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [
+                AllowAny,
+            ]
+        elif (
+            self.action == "destroy"
+            or self.action == "partial_update"
+            or self.action == "update"
+        ):
+            self.permission_classes = [IsOwner | IsAdminUser]
+        else:
+            self.permission_classes = [
+                IsAuthenticated,
+            ]
+
+        return super().get_permissions()
+
 
 class ReviewViewSet(viewsets.ModelViewSet):
     """Вьюсет для модели отзыва"""
@@ -34,3 +54,17 @@ class ReviewViewSet(viewsets.ModelViewSet):
             serializer.save(author=self.request.user)
         except Exception:
             raise ValidationError({"detail": "Данный отзыв уже был оставлен Вами."})
+
+    def get_permissions(self):
+        if (
+            self.action == "destroy"
+            or self.action == "partial_update"
+            or self.action == "update"
+        ):
+            self.permission_classes = [IsOwner | IsAdminUser]
+        else:
+            self.permission_classes = [
+                IsAuthenticated,
+            ]
+
+        return super().get_permissions()
